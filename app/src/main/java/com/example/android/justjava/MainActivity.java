26d05127b98c64518865f713b8e87b6129e8b7cb
imabhishek98 <abhisheknitt@icloud.com>
2017-06-14 17:17:09 +0530

    before name field

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.android.justjava;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.TextView;
-        import java.text.NumberFormat;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 
 public class MainActivity extends AppCompatActivity {
 
     int quantity=0;
+    int price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
         quantity=quantity+1;
         display(quantity);
-        displayPrice(quantity * 5);
 
+
+    }
+
+    private int calculatePrice()
+    {
+        price = quantity * 5;
+        return price;
     }
     public void decrement(View view) {
 
         quantity = quantity - 1;
         display(quantity);
-        displayPrice(quantity * 5);
+
 
 
     }
@@ -47,8 +57,36 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    public void submitOrder(View view){
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        Log.v("MMMMActivity,",  "Has Whipped Cream" + hasWhippedCream);
+        price = calculatePrice();
+
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
+
+    }
+
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+       orderSummaryTextView.setText(message);
+    }
+
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate){
+        String priceMessage= "Name:Kaptain Kunal\nAdd Whipped Cream?  "+ hasWhippedCream;
+        priceMessage += "\n Add Chocolate? "+ hasChocolate;
+
+        priceMessage= priceMessage + "\nQuantity: " + quantity;
+        priceMessage= priceMessage + "\nTotal: $" + price;
+        priceMessage = priceMessage + "\nThank You!";
+        return priceMessage;
+
     }
 
 }
